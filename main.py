869c4b77941068e8bea1d822ced6dfe7c6f7e7fb
nagata4139 <nagata4139@gmail.com>
2020-11-14 12:01:00 +0900

    更新

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-
+import csv
 
 def initialize_database() -> None:
     # このコードは改変しないこと
@@ -19,7 +19,28 @@ def main():
     initialize_database()
 
     # 以下から書き始めること！
-    print("Hello,World")
+
+    # CSVファイルの作成と初期化
+    cfile = open("output.csv", "w")
+    cfile.truncate(0)
+    cfile.close()
+
+    # 生成されたDBファイルに接続
+    root_path = Path(__file__).parent
+    db_path = root_path / "my_movies.db"
+    conn_db = sqlite3.connect(db_path)
+    cursor_db = conn_db.cursor()
+
+    # クエリを生成
+    query = "SELECT genre, SUM(income) as total \
+            FROM my_movies \
+            GROUP BY genre ORDER BY total DESC"
+
+    # 戻り値をCSVに出力
+    for record in cursor_db.execute(query):
+        with open("output.csv", "a", newline="") as cfile:
+            cline = csv.writer(cfile)
+            cline.writerow(record)
 
 if __name__ == '__main__':
     main()
